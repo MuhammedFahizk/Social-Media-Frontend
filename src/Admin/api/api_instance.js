bf696392299ef5de215d1a1f4407d9da1005b649
MuhammedFahizk <fahizk100@gmail.com>
2024-12-07 18:53:53 +0530

    make change to host

--- src/Admin/api/api_instance.js
+++ src/Admin/api/api_instance.js
@@ -1,8 +1,9 @@
 import axios from "axios"; 
 
 export const adminInstance = axios.create({
-    baseURL: 'http://localhost:8080/admin',
+    baseURL: 'https://chat-hive-server.onrender.com/admin',
     withCredentials: true,
+    credentials: 'include',
     headers: {
         'Content-Type': 'application/json',
         timeout: 1000
@@ -13,7 +14,7 @@ export const adminInstance = axios.create({
 
 
 export const userInstance = axios.create({
-    baseURL: 'http://localhost:8080/user', // Base URL for your API
+    baseURL: 'https://chat-hive-server.onrender.com/user', // Base URL for your API
     withCredentials: true, // Ensure cookies are sent with requests if needed
     headers: {
         'Content-Type': 'application/json', // Specify the content type for requests
